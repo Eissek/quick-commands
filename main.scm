9b0999d3c64ee4a9e39a878c30211e636d040764
Eissek <sane.gez@gmail.com>
2015-09-17 22:53:25 +0100

    Added insert commands procedure

--- main.scm
+++ main.scm
@@ -12,10 +12,16 @@
 (define (hello)
   (print "Hello World"))
 
+(define this-path
+  (lambda ()
+    (print (current-directory))))
 
+(define qcommands-db
+  (open-database "/media/sf_Projects/Chicken/quick-commands/resources/qcommands.db"))
 
 (define (add-command . args)
   ;; args stucture (command description tag1 tag2...)
+  ;; (qcommands-db)
   (cond ((>= (length args) 3)
          (print "Its more than or equal to 3")
          ;; (print args)
@@ -29,9 +35,28 @@
          (print "its two"))
         (else (print "Wrong number of arguments."))))
 
-(define (delete-command .args))
-(define (store-in-db .args))
-(define (search-commands))
-(define (list-stored-commands))
+
+
+;; insert command with tags
+(define (insert-cmd-in-db cmd desc . tags)
+  (let ((insert-cmd (prepare qcommands-db "INSERT INTO commands (Command, Description, Tags) VALUES (?,?,?)")))
+    (execute insert-cmd cmd desc (string-join tags))
+    (finalize! qcommands-db insert-cmd)))
+
+
+;; insert command with no tags
+;; (define (delete-command .args))
+;; (define (store-in-db .args))
+;; (define (search-commands))
+(define select-all
+  (prepare qcommands-db "SELECT Command, Description, Tags FROM commands"))
+
+(define (print-commands . cmd)
+  (pp (string-join cmd " | ")))
+
+(define (list-stored-commands .)
+  ;; (qcommands-db)
+  (for-each-row print-commands select-all)
+  (finalize! qcommands-db select-all))
 
 
