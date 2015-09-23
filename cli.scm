d4bf171e39e24aaded840752836f9a3ce80f0f7a
Eissek <sane.gez@gmail.com>
2015-09-23 15:18:56 +0100

    Added cli.scm source

---
+++ cli.scm
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; cli.scm
+; Created by Eissek
+; 21 September 2015
+;
+; Provides a Command Line Interface for the
+; quickCommands application
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
