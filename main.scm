ee550276f48bc03892889c253c5ced15afa53258
Eissek <sane.gez@gmail.com>
2015-09-30 16:56:10 +0100

    changed print commands to add seperator

--- main.scm
+++ main.scm
@@ -79,11 +79,12 @@
   (let ((new-cmd (append
                   (list (number->string (car cmd)))
                   (cdr cmd)
-                  )))
+                  '("\n"))))
     ;; (print new-cmd)
     ;; (pp (string-join new-cmd " | "))
     (string-join new-cmd " | ")
     ;; new-cmd
+    ;; (append new-cmd '("\n"))
     ;; (write (string-join new-cmd " | "))
     ;; (newline)
     
